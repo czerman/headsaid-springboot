625397bcc407d306c6cc6ac2b847e12c1b780cf2
czerman <45125742+czerman@users.noreply.github.com>
2018-12-02 22:06:34 +0300

    create project

---
+++ headsaid-springboot/src/main/java/headsaid/springboot/rest/headsaidspringboot/HeadsaidSpringbootApplication.java
@@ -0,0 +1,12 @@
+package headsaid.springboot.rest.headsaidspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadsaidSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadsaidSpringbootApplication.class, args);
+	}
+}
